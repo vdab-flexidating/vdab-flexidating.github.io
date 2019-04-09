7b26c5871c45036f81f70224b4b27111f2c24854
Grietdk <griet.dekeyser@vdabcampus.be>
2019-04-09 14:37:59 +0200

    met sterrenbeeld

--- assets/profiel.js
+++ assets/profiel.js
@@ -1,10 +1,12 @@
-/* global vars */
+var rooturl = "https://scrumserver.tenobe.org/scrum/api";
 var profiel;
+
+// TO DO VERBORGEN VELDEN
 
 
 /* DOM elementen */
+var eProfiel = document.querySelector('div.profiel')
 var eNickname = document.querySelector('.profiel .nickname');
-
 var eFamilienaam = document.querySelector('.profiel .familienaam dd');
 var eVoornaam = document.querySelector('.profiel .voornaam dd');
 var eGeboortedatum = document.querySelector('.profiel .geboortedatum dd');
@@ -16,17 +18,56 @@ var eHaarkleur = document.querySelector('.profiel .haarkleur dd');
 var eOogkleur = document.querySelector('.profiel .oogkleur dd');
 var eGrootte = document.querySelector('.profiel .grootte dd');
 var eGewicht = document.querySelector('.profiel .gewicht dd');
+var eFoto = document.querySelector('.profiel .foto img');
+//var eBewerk = document.querySelector('button.bewerk');
+//var eBericht  = document.querySelector('button.bericht');
 
-var eFoto = document.querySelector('.profiel img.foto');
-console.log(eFoto);
-console.log(eNickname);
+// TO DO : aanvullen aan de hand van zoekpagina / local storage
+var eigenProfiel = true;
+var zoekId;
+if (zoekId) {
+	eigenProfiel = false;
 
-var rooturl = "https://scrumserver.tenobe.org/scrum/api";
+	//getProfielById(id);
 
+	//toon button 'stuur bericht'
+	var eButtonBericht = document.createElement('button');
+	var tButtonBericht = document.createTextNode('Stuur bericht');
+	eButtonBericht.appendChild(tButtonBericht);
+	eButtonBericht.className = "btn btn-dark bericht";
+	eProfiel.appendChild(eButtonBericht);
+
+	//event button 'stuur bericht'
+	// eButtonBericht.addEventListener('click', function () {
+	// 	location.href = berichten.html; 
+	// });
+
+} else
+if (eigenProfiel) {
+	//haal gegevens profiel uit local storage (1 of meerdere keys?)
+	//profiel = localStorage.profiel;
+	
+	//getIngevuldProfiel(profiel);
+
+	//toon button 'bewerk profiel'
+	var eButtonBewerk = document.createElement('button');
+	var tButtonBewerk = document.createTextNode('Bewerk profiel');
+	eButtonBewerk.appendChild(tButtonBewerk);
+	eButtonBewerk.className = "btn btn-dark bewerk";
+	eProfiel.appendChild(eButtonBewerk);
+
+	//event button 'bewerk profiel'
+	// eButtonBewerk.addEventListener('click', function () {
+	// 	location.href = profielbewerken.html;
+	// });
+}
+
+//voorlopig vast id
 getProfielById(5);
 
 function getProfielById(id) {
-	let profielId = 5;
+	/* haalt profiel op o.b.v. id */
+	let profielId = id;
 	let url = rooturl + '/profiel/read_one.php?id=' + profielId;
 
 	fetch(url)
@@ -35,33 +76,73 @@ function getProfielById(id) {
 	    })
 	    .then(function (data) {
 	        profiel = data;
-	        console.log(profiel);
-	   	
-	   		//TO DO buiten fetch plaatsen
-	   		eNickname.innerHTML = profiel.nickname;
 
-			eFamilienaam.innerHTML = profiel.familienaam;
-			eVoornaam.innerHTML = profiel.voornaam;
-			eGeboortedatum.innerHTML = profiel.geboortedatum;
+	        if (profiel.id) {
+	        	getIngevuldProfiel(profiel);
+	        }
+	        else {
 
-			eEmail.innerHTML = profiel.email;
-			eBeroep.innerHTML = profiel.beroep;
-			eSexe.innerHTML = profiel.sexe;
-			eHaarkleur.innerHTML = profiel.haarkleur;
-			eOogkleur.innerHTML = profiel.oogkleur;
-			eGrootte.innerHTML = profiel.grootte;
-			eGewicht.innerHTML = profiel.gewicht;
-
-			//Foto's ?????
-			//eFoto.src = profiel.foto;
+	        }
+	   		
 	    })
 	    .catch(function (error) {
 	        console.log(error);
 	    });
-
-	console.log(profiel);
 }
 
-// sterrenbeeld nog invullen
+function getIngevuldProfiel(profiel) {
+	/* vult gegevens profiel aan op basis van een array */
 
-// verborgen velden aanpassen
+	eNickname.innerHTML = profiel.nickname;
+
+	eFamilienaam.innerHTML = profiel.familienaam;
+	eVoornaam.innerHTML = profiel.voornaam;
+	eGeboortedatum.innerHTML = profiel.geboortedatum;
+
+	eSterrenbeeld.innerHTML = getSterrenbeeld(profiel.geboortedatum);
+
+	eEmail.innerHTML = profiel.email;
+	eBeroep.innerHTML = profiel.beroep;
+	eSexe.innerHTML = profiel.sexe;
+	eHaarkleur.innerHTML = profiel.haarkleur;
+	eOogkleur.innerHTML = profiel.oogkleur;
+	eGrootte.innerHTML = profiel.grootte;
+	eGewicht.innerHTML = profiel.gewicht;
+
+	// Foto
+	//eFoto.src = profiel.foto;
+}
+
+function getSterrenbeeld(geboortedatum) {
+	/* return sterrenbeeld op basis van geboortedatum */
+	let sterrenbeelden = ["Waterman", "Vissen", "Ram", "Stier", "Tweelingen", "Kreeft", "Leeuw", "Maagd", "Weegschaal", "Schorpioen", "Boogschutter", "Steenbok"]
+	console.log(geboortedatum);
+	let maand = geboortedatum.split('-')[1];
+	let dag = geboortedatum.split('-')[2];
+
+	if((maand == 1 && dag <= 20) || (maand == 12 && dag >=22)) {
+    	return sterrenbeelden[0];
+  	} else if ((maand == 1 && dag >= 21) || (maand == 2 && dag <= 18)) {
+    	return sterrenbeelden[1];
+  	} else if((maand == 2 && dag >= 19) || (maand == 3 && dag <= 20)) {
+    	return sterrenbeelden[2];
+  	} else if((maand == 3 && dag >= 21) || (maand == 4 && dag <= 20)) {
+    	return sterrenbeelden[3];
+  	} else if((maand == 4 && dag >= 21) || (maand == 5 && dag <= 20)) {
+    	return sterrenbeelden[4];
+  	} else if((maand == 5 && dag >= 21) || (maand == 6 && dag <= 20)) {
+    	return sterrenbeelden[5];
+  	} else if((maand == 6 && dag >= 22) || (maand == 7 && dag <= 22)) {
+    	return sterrenbeelden[6];
+  	} else if((maand == 7 && dag >= 23) || (maand == 8 && dag <= 23)) {
+    	return sterrenbeelden[7];
+  	} else if((maand == 8 && dag >= 24) || (maand == 9 && dag <= 23)) {
+    	return sterrenbeelden[8];
+ 	} else if((maand == 9 && dag >= 24) || (maand == 10 && dag <= 23)) {
+    	return sterrenbeelden[9];
+  	} else if((maand == 10 && dag >= 24) || (maand == 11 && dag <= 22)) {
+    	return sterrenbeelden[10];
+  	} else if((maand == 11 && dag >= 23) || (maand == 12 && dag <= 21)) {
+		return sterrenbeelden[11];
+}
+}
