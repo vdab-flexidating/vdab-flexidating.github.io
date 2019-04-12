bee83882cab29d6862962c0cce1ede2c24888403
Aldo-f <aldo.fieuw@gmail.com>
2019-04-12 14:32:03 +0200

    favorieten zijn getoond

--- assets/profiel.js
+++ assets/profiel.js
@@ -393,25 +393,30 @@ function getFavorieten(titel, profiel, favorieten) {
 
 
 	// alles aanwezig
-	eFavTitel = document.querySelector("#fav-container > h2");
+	// let eFavTitel = document.querySelector("#fav-container > h2");
+	let eFavTitel = document.getElementById("fav-titel");
 
 
-	FavTitel.innerText = profiel.nickname + "'s favorieten:";
+
+	eFavTitel.innerHTML = profiel.nickname + "'s favorieten:";
 
 	for (const favoriet of favorieten) {
-		plaatFavoriet(favoriet)
+		plaatsFavoriet(favoriet)
 
 	}
 }
 
-function plaatFavoriet(favoriet) {
-	let eContainer = document.querySelector("#fav-container > .row");
+function plaatsFavoriet(favoriet) {
+	let eContainer = document.getElementById("fav-row");
 
 	let eDiv = document.createElement('div');
 	eDiv.setAttribute('class', 'col-sm-4');
 
+
 	eA = document.createElement('a');
 	eA.setAttribute('href', '/profiel.html?gebruiker=' + favoriet.anderId);
+
+	// eDiv.appendChild(eA);
 
 	let eCard = document.createElement('div');
 	eCard.setAttribute('class', "card");
@@ -423,15 +428,16 @@ function plaatFavoriet(favoriet) {
 	eCardTitle.setAttribute("class", "card-title");
 	eCardTitle.innerText = favoriet.anderId;
 
-	eCardBody.innerHTML = "Status: " + favoriet.status;
+	let eCardSpan = document.createElement("span");
+	eCardSpan.innerHTML = "Status: " + favoriet.status;
 
 
-	// eCardTitle.appendChild()
 	eCardBody.appendChild(eCardTitle)
+	eCardBody.appendChild(eCardSpan)
 	eCard.appendChild(eCardBody)
 	eA.appendChild(eCard)
 	eDiv.appendChild(eA)
-	eContainer.appendChild(eContainer)
+	eContainer.appendChild(eDiv)
 
 	// placeId = document.getElementById('prof' + i);
 	// eA = document.createElement('a');
